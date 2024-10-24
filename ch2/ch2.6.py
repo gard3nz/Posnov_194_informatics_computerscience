b6827823a72266b36f48f4e1cabfc89fcc78d972
gard3nz <vladposnov@gmail.com>
2024-10-24 22:14:54 +0700

    Create ch2.6.py

---
+++ ch2/ch2.6.py
@@ -0,0 +1,8 @@
+m = int(input())
+n = int(input())
+if m > n:
+    print("Number " + str(m) + " > " + str(n))
+if m < n:
+    print("Number " + str(m) + " < " + str(n))
+else:
+    print("The numbers are equal")

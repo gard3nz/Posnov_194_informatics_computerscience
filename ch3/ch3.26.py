0d93a1f74360b5102012471d57794b3a65a7598d
gard3nz <vladposnov@gmail.com>
2024-10-24 23:03:38 +0700

    Update ch3.26.py

--- ch3/ch3.26.py
+++ ch3/ch3.26.py
@@ -1,8 +1,6 @@
-def wallis_formula(n):
-    product = 1.0
-    for i in range(1, n + 1):
-        product *= (4 * i**2) / (4 * i**2 - 1)
-    return 2 * product
-n = 100000
-pi_approximation = wallis_formula(n)
-print(f"Приближенное значение π с использованием {n} членов: {pi_approximation}")
+pi = 2.0
+for i in range(1, 100000):
+    left = (2.0 * i)/(2.0 * i - 1.0)
+    right = (2.0 * i)/(2.0 * i + 1.0)
+    pi = pi * left * right
+print(pi)

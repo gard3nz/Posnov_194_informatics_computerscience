1f425dbcaf307885956f6f8f1f41e22e4e77179e
gard3nz <vladposnov@gmail.com>
2024-10-25 02:27:40 +0700

    Create ch.🌷.py

---
+++ ch5/ch.🌷.py
@@ -0,0 +1,4 @@
+flowers_set1 = ["rose", "jasmine", "lily"]
+flowers_set2 = ["orchid", "tulip", "violet", "daisy"]
+flowers_set3 = ["lavender", "sunflower"]
+print(2**(len(flowers_set1))+2**(len(flowers_set2))+2**(len(flowers_set3)))

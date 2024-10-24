892d69bbf48128cd8875f46e54d9f3c01e2a8761
gard3nz <vladposnov@gmail.com>
2024-10-25 01:48:42 +0700

    Create ch4.1.py

---
+++ ch4/ch4.1.py
@@ -0,0 +1,25 @@
+def scalar(a, b):
+    return sum(a[i] * b[i] for i in range(3))
+
+def vector(a, b):
+    return [
+        a[1] * b[2] - a[2] * b[1],
+        a[2] * b[0] - a[0] * b[2],
+        a[0] * b[1] - a[1] * b[0]
+    ]
+
+def scalar3(a, b, c):
+    sc = scalar(b, c)
+    return scalar(a, [sc] * 3)
+
+def vector3(a, b, c):
+    vc = vector(b, c)
+    return vector(a, vc)
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = [7, 8, 9]
+print("Скалярное произведение:", scalar(a, b))
+print("Векторное произведение:", vector(a, b))
+print("Скалярное смешанное произведение:", scalar3(a, b, c))
+print("Векторное смешанное произведение:", vector3(a, b, c))
